23cdd62f8556c3bc7e24b16e2ab6c2e8c69dd93a
Sarah Kreidler <sarah5280@gmail.com>
2012-02-20 22:02:09 +0000

    moved to enums

--- src/edu/ucdenver/bios/webservice/common/enums/HorizontalAxisLabelEnum.java
+++ src/edu/ucdenver/bios/webservice/common/enums/HorizontalAxisLabelEnum.java
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package edu.ucdenver.bios.webservice.common.enumclasses;
+package edu.ucdenver.bios.webservice.common.enums;
 
 /**
  * Enum object referred in 'PowerCurveDescription' class.
